6977e4c273026c2ffb6c7530060904cb6dc8f9b8
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-11-16 17:41:51 -0600

    creates reset button

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,30 @@
 const container = document.querySelector('.container') // selects container div 
-let numOfDivs = prompt('Enter grid size. Maximum 100 x 100');  //takes user input 
-function createDivs(num) {
+const resetButton = document.querySelector('button'); // selects button 
+function createDivs(num) { // takes a number and creates that many divs with class gridDiv
 for (let i = 0; i < num; i++) { // loop until i = the input number 
     const gridDivs = document.createElement('div');
     gridDivs.classList.add('gridDiv');
     container.appendChild(gridDivs);
 }
 }
-createDivs(numOfDivs);
+resetButton.addEventListener('click', function(e) { // when the reset button is clicked
+    document.querySelectorAll('.gridDiv').forEach(gridDiv => {
+        container.removeChild(gridDiv)
+    })
+    let input = prompt('Enter grid size'); // prompt player to input the grid size they want
+    if (input <= 100 && input > 0) {  // if the input is less than 100 but more then 0 
+        let num = input * input // finds area to create correct number of divs
+        createDivs(num) 
+        let px = 500 / input; // finds what the width and height of each box should be to fit in 500 x 500 container
+        document.querySelectorAll('.gridDiv').forEach(gridDiv => { // for each div with a class of grid div
+            gridDiv.addEventListener('mouseover', function(e)  { // look for when div is hovered over
+                e.target.classList.add('hover'); // add class of hover which changes background to black
+               })
+            gridDiv.style.width = `${px}px`; // adds correct width and height
+        gridDiv.style.height = `${px}px`; 
+        })
+        container.style.gridTemplateColumns = `repeat(${input}, ${px}px [col-start])`; // makes grid correct size
+    } else { // if number is to big or small give an error
+        alert('error enter a number between 1 and 100')
+    }
+})
